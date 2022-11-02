25d3b8f2ca958afeb4c05d8d5d568d59ad8b3a93
CodeCommit-SGS-Y.Mu-at-741165279592 <yua.mu@sony.com>
2022-11-02 13:09:48 +0800

    attack project commit

---
+++ src/main/java/com/xxe/attack/web/ReciveDataController.java
@@ -0,0 +1,20 @@
+package com.xxe.attack.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReciveDataController {
+
+    Log logger = LogFactory.getLog(ReciveDataController.class);
+
+    @GetMapping("/attack/{param}")
+    public String attack(@PathVariable String param) {
+        logger.info("recive data ------->" + param);
+        return "success";
+    }
+
+}
